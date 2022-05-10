ff692b3a1c6d1b0d149e9c3f0e51e50689cce819
b0463 <hkimpr40@gmail.com>
2022-05-10 04:23:16 -0300

    header/footer resp

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
     <header-main/>
+    <div class="space"></div>
+    <footer-main/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HeaderMain from "./components/HeaderMain.vue";
+import FooterMain from "./components/FooterMain.vue";
 export default defineComponent({
     name: 'App',
     components: {
-        HeaderMain
+        HeaderMain,
+        FooterMain
     }
 });
 </script>
-<style>
+<style scoped>
+.space {
+    height: 20vh;
+}
 </style>
