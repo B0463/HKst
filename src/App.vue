148e6df52c1c88a8af3c6f8cb052bacb071f7b8f
b0463 <hkimpr40@gmail.com>
2022-05-10 02:18:04 -0300

    init HKst in vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <header-main/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeaderMain from "./components/HeaderMain.vue";
+export default defineComponent({
+    name: 'App',
+    components: {
+        HeaderMain
+    }
+});
+</script>
+<style>
+</style>
---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="box has-background-black-ter header">
+        <div class="columns is-vcentered is-mobile">
+            <figure class="image column headerLogoFig">
+                <img class="headerLogo" src="../assets/logo.png">
+            </figure>
+            <h1 class="column title headerTitle">HKst</h1>
+        </div>
+        <nav class="headerNav">
+            
+        </nav>
+    </header>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "HeaderMain"
+})
+</script>
+
+<style scoped>
+.headerLogo {
+    width: 128px;
+}
+.headerTitle {
+    color: #f0f0f0
+}
+.header {
+    border-radius: 0;
+}
+</style>
