e521c096ba7c1cd92149c7530a35d5e3c941ff49
b0463 <hkimpr40@gmail.com>
2022-05-11 02:41:56 -0300

    create errors

---
+++ src/errors/404/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>erro 404, não foi possivel achar o local especifico</h1>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "NotFound"
+})
+</script>
+
+<style scoped>
+
+</style>
