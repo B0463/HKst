0210de589d539510547769ea0bb41c97320e501f
b0463 <hkimpr40@gmail.com>
2022-05-10 23:45:13 -0300

    init in vite vue project

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-    <header-main/>
-    <div class="space"></div>
-    <footer-main/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HeaderMain from "./components/HeaderMain.vue";
-import FooterMain from "./components/FooterMain.vue";
-export default defineComponent({
-    name: 'App',
-    components: {
-        HeaderMain,
-        FooterMain
-    }
-});
-</script>
-<style scoped>
-.space {
-    height: 20vh;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
