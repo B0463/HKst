e73b83e4dbd1b8de010955e9719b2bad9a1a7a52
b0463 <hkimpr40@gmail.com>
2022-05-11 02:42:07 -0300

    add deploy.sh

---
+++ deployer/deploy.sh
@@ -0,0 +1,14 @@
+cd ..
+rm -r public
+rm -r src
+rm index.html
+rm .gitignore
+rm package-lock.json
+rm package.json
+rm README.md
+rm tsconfig.json
+rm tsconfig.nome.json
+rm vite.config.ts
+cd server
+rm -r src
+rm tsconfig.json
