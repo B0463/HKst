56a3e8559c52ddf703bf3bead3b66594a81c172c
b0463 <hkimpr40@gmail.com>
2022-05-12 04:41:21 -0300

    SPA init and main home init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <header-main/>
+    <home-main/>
+    <footer-main/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HeaderMain from "./components/HeaderMain.vue";
+import FooterMain from "./components/FooterMain.vue";
+import HomeMain from "./components/HomeMain.vue";
+export default defineComponent({
+    name: 'App',
+    components: {
+        HeaderMain,
+        FooterMain,
+        HomeMain
+    }
+});
+</script>
+<style scoped>
+</style>
