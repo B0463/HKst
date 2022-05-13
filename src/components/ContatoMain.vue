58d4b823da919b73cc7599602f177e2b7e4fecab
b0463 <hkimpr40@gmail.com>
2022-05-13 02:25:20 -0300

    contato update

--- src/components/ContatoMain.vue
+++ src/components/ContatoMain.vue
@@ -1,6 +1,97 @@
 <template>
     <main>
-        <div class="space"></div>
+        <div class="boxes">
+            <a href="https://github.com/B0463" target="_blank">
+                <div class="box contCard backGitHub">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-white">GitHub</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-white">B0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://www.clubedohardware.com.br/profile/1141630-b0463/" target="_blank">
+                <div class="box contCard backCDH">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-light">Clube Do Hardware</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-light">b.0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://cursos.alura.com.br/user/Henr5k" target="_blank">
+                <div class="box contCard backAlura">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-light">Alura</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-light">Henr5k</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://www.tiktok.com/@b.0463" target="_blank">
+                <div class="box contCard backTok">
+                    <div class="rede">
+                        <h4 class="title is-4 textTok">TikTok</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 textTok">@b.0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://www.instagram.com/b.0463/" target="_blank">
+                <div class="box contCard backInsta">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-light">Instagram</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-light">@b.0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://twitter.com/b_0463" target="_blank">
+                <div class="box contCard has-background-info">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-light">Twitter</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-light">@b_0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://www.youtube.com/channel/UCWzoko_gQ_6nRqehIifCmNA" target="_blank">
+                <div class="box contCard backRed">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-white">YouTube</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-white">b.0463</h3>
+                    </div>
+                </div>
+            </a>
+            <a target="_blank">
+                <div class="box contCard has-background-primary">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-black">Gmail</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-black">hkimpr40@gmail.com</h3>
+                    </div>
+                </div>
+            </a>
+            <a href="https://discord.gg/2d77HA5NjM" target="_blank">
+                <div class="box contCard backDis">
+                    <div class="rede">
+                        <h4 class="title is-4 has-text-white">Discord</h4>
+                    </div>
+                    <div class="name">
+                        <h3 class="title is-3 has-text-white">Ceira Server</h3>
+                    </div>
+                </div>
+            </a>
+        </div>
     </main>
 </template>
 
